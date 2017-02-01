1c8d4b3b34aba8db0c508711e38efec902409b12
Andrew Drozdov <andrew@mrdrozdov.com>
2017-01-31 23:06:23 -0500

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytorch-extras',
-      version='0.1.1',
+      version='0.1.2',
       description='A handful of extensions to the pytorch library.',
       url='http://github.com/mrdrozdov/pytorch-extras',
       author='Andrew Drozdov',
