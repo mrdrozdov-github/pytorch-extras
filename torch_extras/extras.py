d4e37a41e2c47cc2aef08f8f4e8955712910fd1d
Andrew Drozdov <andrew@mrdrozdov.com>
2017-02-01 15:00:54 -0500

    Augment code with docs.

--- torch_extras/extras.py
+++ torch_extras/extras.py
@@ -4,6 +4,20 @@ from torch.autograd import Variable
 
 
 def expand_along(var, mask):
+    """ Useful for selecting a dynamic amount of items from different
+        indexes using a byte mask.
+
+        ```
+        import torch
+        import torch_extras
+        setattr(torch, 'expand_along', torch_extras.expand_along)
+
+        var = torch.Tensor([1, 0, 2])
+        mask = torch.ByteTensor([[True, True], [False, True], [False, False]])
+        torch.expand_along(var, mask)
+        # (1, 1, 0)
+        ```
+    """
     indexes = torch.range(0, var.size(0) - 1).view(-1, 1).repeat(1, mask.size(1))
     _mask = indexes[mask].long()
     if isinstance(var, Variable):
@@ -12,12 +26,35 @@ def expand_along(var, mask):
 
 
 def expand_dims(var, dim=0):
+    """ Is similar to [numpy.expand_dims](https://docs.scipy.org/doc/numpy/reference/generated/numpy.expand_dims.html).
+
+        import torch
+        import torch_extras
+        setattr(torch, 'expand_dims', torch_extras.expand_dims)
+
+        var = torch.range(0, 9).view(-1, 2)
+        torch.expand_dims(var, 0).size()
+        # (1, 5, 2)
+    """
     sizes = list(var.size())
     sizes.insert(dim, 1)
     return var.view(*sizes)
 
 
 def select_item(var, index):
+    """ Is similar to `[var[row,col] for row, col in enumerate(index)]`.
+
+        ```
+        import torch
+        import torch_extras
+        setattr(torch, 'select_item', torch_extras.select_item)
+
+        var = torch.range(0, 9).view(-1, 2)
+        index = torch.LongTensor([0, 0, 0, 1, 1])
+        torch.select_item(var, index)
+        # [0, 2, 4, 7, 9]
+        ```
+    """
     index_mask = index.view(-1, 1).repeat(1, var.size(1))
     mask = torch.range(0, var.size(1) - 1).long()
     mask = mask.repeat(var.size(0), 1)
